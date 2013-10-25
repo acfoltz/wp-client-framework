a1a1003a4109dd04f0386b79623df954382809d8
Brandon Hubbard <bkhubbard@gmail.com>
2013-10-24 22:21:45 -0700

    Adding some more jetpack code

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -10,12 +10,20 @@
  * Add theme support for Infinite Scroll.
  * See: http://jetpack.me/support/infinite-scroll/
  */
-function _s_jetpack_setup() {
+ 
+// Force disable all modules by default
+add_filter( 'jetpack_get_default_modules', '__return_empty_array' );
+
+// Force Selecting Publicise
+// add_filter( 'publicize_checkbox_default', '__return_false' );
+ 
+// Infinite Scroll Support
+function client_jetpack_setup() {
         add_theme_support( 'infinite-scroll', array(
                 'container' => 'main',
                 'footer'    => 'page',
         ) );
 }
-add_action( 'after_setup_theme', '_s_jetpack_setup' );
+add_action( 'after_setup_theme', 'client_jetpack_setup' );
 
 
